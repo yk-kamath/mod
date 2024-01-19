de2c4896f5fd35cfde5935a4cca2af40b476afe1
Dr. Ernesto Lee <socrates73@gmail.com>
2024-01-19 13:36:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ api_key = st.secrets["openai_secret"]
 client = OpenAI(api_key=api_key)
 
 # Streamlit UI components
-st.title("OpenAI Moderation API Demo")
+st.title('''Dr. Ernesto Lee - CAI 2300C - 25  Introduction to Natural Language Processing at Miami Dade College - Kendall Campus''')
 
 user_input = st.text_area("Enter text to moderate")
 
